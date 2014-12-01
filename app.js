bfecdf20f19336201d264169f9d67ae42252551b
Zev Youra <zevyoura@gmail.com>
2014-11-30 22:26:32 -0800

    basic display of permit locations

--- app.js
+++ app.js
@@ -6,39 +6,39 @@ var INDEX_NAME =  9,
     INDEX_LAT = 22
     INDEX_LON = 23;
 
-var jsonLoaded = function(error, data) {
-    d3.select('.loading').remove();
-    d3.select('.controls').classed('hidden', false);
+// var jsonLoaded = function(error, data) {
+//     d3.select('.loading').remove();
+//     d3.select('.controls').classed('hidden', false);
 
-    if (error) {
-        d3.select('body').append('div')
-            .text("Could not load data")
-            .classed('error', true);
-        return;
-    }
+//     if (error) {
+//         d3.select('body').append('div')
+//             .text("Could not load data")
+//             .classed('error', true);
+//         return;
+//     }
 
-    // basic chart setup
-    var body = d3.select('body');
-    var chart = body.append('div');
-    chart.classed('chart', true);
-    var rows = chart.selectAll('.row').data(data.data)
-        .enter().append('div');
-    rows.classed('row', true);
-    var getName = function(d,i,c) {
-        return d[INDEX_NAME];
-    };
-    rows.text(getName);
+//     // basic chart setup
+//     var body = d3.select('body');
+//     var chart = body.append('div');
+//     chart.classed('chart', true);
+//     var rows = chart.selectAll('.row').data(data.data)
+//         .enter().append('div');
+//     rows.classed('row', true);
+//     var getName = function(d,i,c) {
+//         return d[INDEX_NAME];
+//     };
+//     rows.text(getName);
 
-    // set up basic text / name filter
-    var filterInput = d3.select('.controls input.filter');
-    filterInput.on('input', function(e) {
-        rows.classed('hidden', function(d,i,c) {
-            var filterProcessed = filterInput[0][0].value.toLowerCase();
-            var nameProcessed = d[INDEX_NAME].toLowerCase();
-            return nameProcessed.indexOf(filterProcessed) == -1;
-        });
-    });
-};
+//     // set up basic text / name filter
+//     var filterInput = d3.select('.controls input.filter');
+//     filterInput.on('input', function(e) {
+//         rows.classed('hidden', function(d,i,c) {
+//             var filterProcessed = filterInput[0][0].value.toLowerCase();
+//             var nameProcessed = d[INDEX_NAME].toLowerCase();
+//             return nameProcessed.indexOf(filterProcessed) == -1;
+//         });
+//     });
+// };
 
 //d3.json('/sf-mobile-food-permits.json', jsonLoaded);
 
@@ -47,15 +47,14 @@ d3.json('/data/bay.json', function(error, data) {
     if (error) {
         console.log("An error occured: " + error);
     } else {
-        console.log("Data loaded");
-        console.log(data);
         var width = window.innerWidth,
             height = window.innerHeight;
 
         var svg = d3.select("body").append("svg")
             .attr("width", width)
             .attr("height", height);
-        var geoData = topojson.feature(data, data.objects.bayareageo);
+
+        var geoData = topojson.feature(data.topo, data.topo.objects.geo);
 
         // get projection frame; based on mbostock SO answer:
         // http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
@@ -68,11 +67,41 @@ d3.json('/data/bay.json', function(error, data) {
             t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
         projection.scale(s).translate(t);
 
-        svg.selectAll(".subunit")
+        svg.selectAll(".geounit")
                 .data(geoData.features)
             .enter().append("path")
-                .attr("class", function(d) { return "subunit " + d.properties.name.toLowerCase().replace(' ', '-'); })
-                .attr("d", path);
+                .attr("class", function(d) { 
+                    return "geounit " + d.properties["AREA_NAME"].toLowerCase().replace(' ', '-');
+                }).attr("d", path);
+
+        var streetData = topojson.feature(data.topo, data.topo.objects.streets);
+
+        svg.selectAll(".street")
+                .data(streetData.features)
+            .enter().append("path")
+                .attr("class", function(d) {
+                    var streetLabel = "null-st";
+                    if (d.properties["STREET"]) {
+                        streetLabel = d.properties["STREET"].toLowerCase().replace(/[^\w]/g, '-');
+                        if (streetLabel.search(/[\d]/) == 0) {
+                            streetLabel = 'num-' + streetLabel;
+                        }
+                    }
+                    if (d.properties["ST_TYPE"]) {
+                        streetLabel +=  '-' + d.properties["ST_TYPE"].toLowerCase();
+                    }
+                    return "street " + streetLabel;
+                }).attr("d", path);
+
+        svg.selectAll(".permit")
+                .data(data["food-permits"].data)
+            .enter().append("circle")
+                .classed("permit", true)
+                .attr("cx", function(d) {
+                    return projection([d[INDEX_LON], d[INDEX_LAT]])[0];
+                }).attr("cy", function(d) {
+                    return projection([d[INDEX_LON], d[INDEX_LAT]])[1];
+                }).attr("r", 5);
     }
 });
 
